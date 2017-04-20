7464cbf94f91ebfca641a02d8ac2227f9fab5259
nickngweipeng <15056201@myrp.edu.sg>
2017-04-20 13:24:05 +0800

    ok

---
+++ app/src/main/java/com/example/nickng/demoactivities/DemoActivities2.java
@@ -0,0 +1,28 @@
+package com.example.nickng.demoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class DemoActivities2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activities2);
+        // Get the intent so as to get the "things" inside the intent
+        Intent i = getIntent();
+        // Get the String array named "info" we passed in
+        String[] info = i.getStringArrayExtra("info");
+        // Get the TextView object
+        TextView tv1 = (TextView) findViewById(R.id.textView1);
+        // Display the name and age on the TextView
+        tv1.setText("You are " + info[0] + ", age " + info[1]);
+    }
+
+}
